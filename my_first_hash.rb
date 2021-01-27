9ee635f31a0935edee93802b6129d5a7b45f1a98
olga gomelskiy <olichka23.85@gmail.com>
2021-01-27 12:43:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,3 +25,19 @@ def retrieval
 end
 
 def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+  shipping_manifest["muskets"]=>2# add 2 muskets to the shipping_manifest hash below
+
+
+
+  shipping_manifest["gun powder"]=>4# add 4 gun powder to the shipping_manifest hash below
+
+
+
+  return# return the shipping_manifest hash below
+
+end
